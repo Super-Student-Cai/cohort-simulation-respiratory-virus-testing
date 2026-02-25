00d7bd3c84abeabc09ffa198bef7e27831ea4195
Super-Student-Cai <zhiqing.cai@stu.njmu.edu.cn>
2026-02-25 13:21:25 +0800

    Initial commit: cohort simulation framework

---
+++ cohort-simulation-respiratory-virus-testing/scripts/run.R
@@ -0,0 +1,7 @@
+# Run the cohort simulation + analysis pipeline
+# Usage (in R): source("scripts/run.R")
+#
+# Tip: If you do NOT want to auto-install packages, comment out the install section
+# inside R/cohort_simulation_framework.R.
+
+source(file.path("R", "cohort_simulation_framework.R"))
